ead36c6dc735fc6c596598cc8101fd87b95e6091
Marcos Irazabal <marcos.irazabal@accenture.com>
2022-09-22 18:10:33 -0300

    seccion 3 terminada

--- callback-hell.js
+++ callback-hell.js
@@ -8,6 +8,8 @@ const salarios = [
     { id:2, sueldo:1200 }
 ]
 
+const id =3;
+
 const getEmpleado = (id, funcion_callback) => {
     const empleado = empleados.find( emple=>emple.id === id);
     //si existe un empleado que cumpla con la condicion anterior
@@ -21,10 +23,29 @@ const getEmpleado = (id, funcion_callback) => {
 
 //un callback es pasar por parametro una funcion que voy a utilizar
 //hacemos la llamada a getEmpleado y definimos que hace la funcion callback
-getEmpleado( 4 , (error,empleadito)=> {
+getEmpleado( id , (error,empleadito)=> {
     if( error ){
         console.log(error)
     }else{
         console.log(empleadito);
+    }
+})
+
+console.log("separador ----------------------------------------------------");
+
+const getSalario = (id, funcion_callback) => {
+    const salario = salarios.find( salario=>salario.id === id);
+    if (salario){
+        funcion_callback(null,salario); 
+    }else{
+        funcion_callback("no existe salario  para persona con id: "+id);
+    }
+}
+
+getSalario( id , (error,plata)=> {
+    if( error ){
+        console.log(error)
+    }else{
+        console.log(plata);
     }
 })
