299ba009ae428997cff03a5ea93b4f53d7cc67b2
Marcos Irazabal <marcos.irazabal@accenture.com>
2022-11-14 15:28:46 -0300

    terminada seccion 11

--- 07-rest-server/controllers/auth.controller.js
+++ 07-rest-server/controllers/auth.controller.js
@@ -1,7 +1,8 @@
-const { response, request } = require('express');
+const { response, request, json } = require('express');
 const User = require("../models/user.model.js")
 const generateJWT  = require("../helpers/jwt-generator.js")
 const bcrypt = require("bcryptjs") ;
+const { googleVerify } = require('../helpers/google-verify.js');
 
 const login = async(req, res = response) => {
 
@@ -40,4 +41,52 @@ const login = async(req, res = response) => {
     });
 }
 
-module.exports={ login }
+const googleSingIn=async(req, res = response)=>{
+    const {id_token} = req.body;
+
+    try {
+        const googleUser=await googleVerify(id_token)
+        const {name,image,email} = googleUser;
+        console.log(googleUser)
+
+        let user = await User.findOne({email});
+        if(!user){
+            const userValues={
+                name,
+                email,
+                password:"123456",
+                image,
+                google:true
+            }
+        user =new User(userValues)
+        await user.save();
+        }
+
+        if(user.status===false){
+            return res.status(400).json({
+                msg:"blocked user"
+            })
+        }
+
+        //genero JSON web token
+        const token = await generateJWT(user.id)
+
+
+        res.json({
+            msg: 'login OK',
+            user,
+            token
+        });
+        
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            ok:false,
+            msg:"google token cant validate"
+        })
+    }
+
+
+}
+
+module.exports={ login,googleSingIn}
--- 07-rest-server/routes/auth.js
+++ 07-rest-server/routes/auth.js
@@ -1,19 +1,24 @@
 const { Router } = require('express');
 const {check} = require("express-validator")
 const {validateField} = require("../middlewares/field-validator.js")
-const { login } = require("../controllers/auth.controller.js")
+const { login, googleSingIn } = require("../controllers/auth.controller.js")
 
 const router = Router();
 
 
 router.post('/login',[
-        check("email","email no valido").isEmail(),
-        check("email","email es obligatorio").not().isEmpty(),
+    check("email","email no valido").isEmail(),
+    check("email","email es obligatorio").not().isEmpty(),
 
-        check("password","password debe ser mayor a 6 caracteres").isLength({min:6}),
-        validateField],
-    login );
+    check("password","password debe ser mayor a 6 caracteres").isLength({min:6}),
+    validateField],
+login );
 
+router.post('/google',[
+    check("id_token","id token es necesario").not().isEmpty(),
+
+    validateField],
+googleSingIn );
 
 
 
